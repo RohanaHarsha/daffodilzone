ac6fdc104b4320c5a7b975911b9b7d232f5e3469
Rohana Harsha <rohanaharsha@gmail.com>
2025-12-10 11:09:51 +0530

    unintentional file deletion fix

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,13 +2,9 @@
 
 const config = {
   API_URL:
-<<<<<<< HEAD
-   //"https://daffodilzone-b-end.onrender.com" 
-  "http://127.0.0.1:5000"
-=======
    "https://daffodilzone-b-end.onrender.com" 
- // "http://127.0.0.1:5000"
->>>>>>> 927a37052a74df7f229e2e99340edfce87d15ddc
+ //"http://127.0.0.1:5000"
+
 };
 
 export default config;
