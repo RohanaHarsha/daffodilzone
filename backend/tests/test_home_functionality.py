7737167d48bf55b3d64da1d698eb5dd26201d657
Rohana Harsha <rohanaharsha@gmail.com>
2025-12-16 19:36:40 +0530

    main merge

--- backend/tests/test_home_functionality.py
+++ backend/tests/test_home_functionality.py
@@ -1,6 +1,5 @@
 import unittest
 from app import app
-from config import TestingConfig
 
 
 class TestLuxuryHouseUpload(unittest.TestCase):
@@ -29,7 +28,7 @@ class TestLuxuryHouseUpload(unittest.TestCase):
         response = self.client.get("/", data=form_data)
         print(response)
         self.assertEqual(response.status_code, 200)
-        
+      # self.assertIn("Luxury house added successfully", response.get_json()["Havent recieded"])
 
 
 if __name__ == "__main__":
